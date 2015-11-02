69f15707e93788bac0278438b08155ca3b91cab6
marb61a <marb61a@gmail.com>
2015-11-02 14:10:45 +0000

    updated templates and dependencies

--- client/common.js
+++ client/common.js
@@ -2,5 +2,14 @@ Meteor.startup(function () {
     AccountsEntry.config({        
       homeRoute: '/',            
       dashboardRoute: '/'
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
     });
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
+      });
   });
+
+Template.registerHelper('getProfileImg', function(userId){
+	var imgUrl = UserImages.findOne({userId: userId }).image;
+	return imgUrl;
+});
